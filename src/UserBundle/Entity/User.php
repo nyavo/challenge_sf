b8168c867726eba60b915424ec803e853b487650
Ny Avo <nyavo01@gmail.com>
2018-04-18 23:33:34 +0000

    Registration

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
+ */
+class User extends \FOS\UserBundle\Model\User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
---
+++ src/UserBundle/Repository/UserRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 18/04/18
+ * Time: 23:27
+ */
+namespace UserBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class UserRepository
+ */
+class UserRepository extends EntityRepository
+{
+
+}
